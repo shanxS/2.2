cfcadeabdce3643e4c38b14ffa3955dd5079c3ca
shanxS <shanx.shashank@gmail.com>
2015-08-23 22:53:39 +0530

    starting chat

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,6 +1,12 @@
 import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.chat.Chat;
+import org.jivesoftware.smack.chat.ChatManager;
+import org.jivesoftware.smack.chat.ChatMessageListener;
 import org.jivesoftware.smack.debugger.ConsoleDebugger;
 import org.jivesoftware.smack.java7.Java7SmackInitializer;
+import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
@@ -43,11 +49,18 @@ public class Main
             conn1.connect();
             conn1.login("admin", "admin");
 
-            Presence presence = new Presence(Presence.Type.unavailable);
-            presence.setStatus("Gone fishing");
-            conn1.sendStanza(presence);
+            ChatManager chatmanager = ChatManager.getInstanceFor(conn1);
+            Chat newChat = chatmanager.createChat("shanxs@death3", new ChatMessageListener()
+            {
+                @Override
+                public void processMessage(Chat chat, Message message)
+                {
+                    System.out.println("Received message: " + message);
+                }
+            });
 
-            System.out.print(conn1.getHost());
+            newChat.sendMessage("Howdy!");
+
         }
         catch (Exception e)
         {
